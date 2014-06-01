e8372cb922965cef085ef20bc41745489c6b1452
Keith Mancuso <keithmancuso@gmail.com>
2014-06-01 17:41:08 -0400

    Moved js code generation into the service

--- bugherd/services/BugHerdService.php
+++ bugherd/services/BugHerdService.php
@@ -3,14 +3,13 @@ namespace Craft;
 
 class BugHerdService extends BaseApplicationComponent {
 
+
   public function checkAccess() {
 
     $plugin = craft()->plugins->getPlugin('bugHerd');
     $settings = $plugin->getSettings();
 
     $showTab = false;
-
-
 
     if ($settings->getAttribute('frontEnd'))
     {
@@ -40,5 +39,44 @@ class BugHerdService extends BaseApplicationComponent {
 
     return $showTab;
 
+  }
+
+  public function buildCode()
+  {
+
+    $plugin = craft()->plugins->getPlugin('bugHerd');
+    $settings = $plugin->getSettings();
+
+    $user = craft()->userSession->getUser();
+    
+
+    $code = "<script type='text/javascript'>
+
+    var BugHerdConfig = {
+
+        metadata: {
+
+          logged_in: \"true\"
+        },
+
+        feedback: {
+          tab_text: \"".$settings->getAttribute('tabText')."\"
+        },
+
+        reporter: {
+          email: \"".$user->email."\",
+          required: \"true\"
+        }
+      };
+
+(function (d, t) {
+var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+bh.type = 'text/javascript';
+bh.src = '//www.bugherd.com/sidebarv2.js?apikey=".$settings->getAttribute('projectKey')."';
+s.parentNode.insertBefore(bh, s);
+})(document, 'script');
+</script>";
+
+    return $code;
   }
 }
